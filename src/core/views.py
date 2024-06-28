b0c50069698d010726a350b3588fdd9da9d0476e
Mikhail <m.sannikov@itcodegroup.ru>
2024-06-28 09:16:42 +0500

    #6 Добавил README.MD

--- src/core/views.py
+++ src/core/views.py
@@ -1,4 +1,7 @@
+from django.db.models import F
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from core import filters, models, serializers
@@ -17,3 +20,7 @@ class Announcement(ModelViewSet):
         if self.action in ('retrieve', 'list'):
             return serializers.AnnouncementRead
         return serializers.AnnouncementWrite
+
+    def retrieve(self, request: Request, *args: tuple, **kwargs: dict) -> Response:
+        models.Announcement.objects.filter(pk=kwargs['pk']).update(views_amount=F('views_amount') + 1)
+        return super().retrieve(request, *args, **kwargs)
